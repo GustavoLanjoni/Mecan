ea0cea8be2a773fc31b354d5cd0dce8fb3597ac0
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2024-12-07 01:04:20 -0300

    Projeto Mecanica (EletriCar)

---
+++ src/test/java/com/mecanica/mecanica/MecanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecanica.mecanica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecanicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
