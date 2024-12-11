6c555fb8efed8693de94be91b61c536c415d8ce0
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2024-12-11 07:05:03 -0300

    corigindo erros

--- src/main/java/com/mecanica/controller/OrcamentoController.java
+++ src/main/java/com/mecanica/controller/OrcamentoController.java
@@ -1,14 +1,19 @@
 package com.mecanica.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OrcamentoController {
 
-    // Rota para o botão "Orçamento Rápido"
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Carrega o arquivo index.html
+    }
+
     @GetMapping("/orcamento")
     public String orcamentoRapido() {
-        return "orcamento"; // Nome da página HTML (orcamento.html)
+        return "orcamento"; // Carrega o arquivo orcamento.html
     }
 }
